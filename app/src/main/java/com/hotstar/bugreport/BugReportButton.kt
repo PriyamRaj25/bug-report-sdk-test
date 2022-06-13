e5e000d3bd6906bd0fd50263935e4d6b1f22c75a
Priyam Raj <Priyam.Raj.int@hotstar.com>
2022-06-13 14:52:56 +0530

    Update bug report button design

--- app/src/main/java/com/hotstar/bugreport/BugReportButton.kt
+++ app/src/main/java/com/hotstar/bugreport/BugReportButton.kt
@@ -1,23 +1,32 @@
 package com.hotstar.bugreport
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.RelativeLayout
+import androidx.annotation.RequiresApi
+
 import androidx.core.content.ContextCompat.startActivity
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.hotstar.bugreport.R
+
 
 class BugReportButton {
 
     companion object {
-        fun addButton(myContext: Context):FloatingActionButton{
+
+        fun addButton(myContext: Context):Button{
 
             val currentActivity = myContext as Activity
             val parentViewGroup =
                 (currentActivity.findViewById(android.R.id.content) as ViewGroup).getChildAt(0) as ViewGroup
 
-            val floatingButton = FloatingActionButton(myContext)
+            val bugReportButton = Button(myContext)
 
             //TODO different layout types would have different layout_params
 
@@ -25,19 +34,29 @@ class BugReportButton {
                 RelativeLayout.LayoutParams.WRAP_CONTENT,
                 RelativeLayout.LayoutParams.WRAP_CONTENT)
 
-            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT)
             layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP)
+            layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL)
 
-            floatingButton.layoutParams=layoutParams
+            bugReportButton.layoutParams=layoutParams
 
-            parentViewGroup.addView(floatingButton)
-            floatingButton.setOnClickListener {
+            bugReportButton.text = "REPORT"
+            bugReportButton.setTextColor(Color.parseColor("#FFFFFF"))
+
+            bugReportButton.setBackgroundResource(R.drawable.focusable_bug_report_button)
+
+            bugReportButton.isFocusable = true
+            bugReportButton.isFocusableInTouchMode = true
+            bugReportButton.bringToFront()
+            bugReportButton.requestFocus()
+
+            parentViewGroup.addView(bugReportButton)
+            bugReportButton.setOnClickListener {
 
                 val intent = Intent(myContext,BugReportActivity::class.java)
                 startActivity(myContext,intent,null)
             }
 
-            return floatingButton
+            return bugReportButton
         }
     }
 }
